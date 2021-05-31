38d4d0775286f8f62cfb39c2faaf0cd7f6238ccd
mailshurya24 <70964703+mailshurya24@users.noreply.github.com>
2021-05-31 18:42:44 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+def swapFile():
+    file1 = input("What is the name of the first file ?")
+    file2 = input("What is the name of the second file ?")
+
+    with open(file1, 'r') as a:
+        data_a = a.read()
+    with open(file2, 'r') as b:
+        data_b = b.read()
+    with open(file1, 'w') as a:
+        a.write(data_b)
+    with open(file2, 'w') as b:
+        b.write(data_a) 
+        print("Completed! The text in ", file1, "was successfully switched with the text in ", file2, "." )
+swapFile()
